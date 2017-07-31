pmmp/PocketMine-MP

Fixed liquid CPU leaks and some flow issues Liquid still needs a total rewrite, but at least it won't constantly waste CPU anymore.

src/pocketmine/block/Liquid.php
@@ -239,9 +239,9 @@ abstract class Liquid extends Transparent{
 				if($k !== $decay){
 					$decay = $k;
 					if($decay < 0){
-						$this->getLevel()->setBlock($this, new Air(), true);
+						$this->getLevel()->setBlock($this, new Air(), true, true);
 					}else{
-						$this->getLevel()->setBlock($this, Block::get($this->id, $decay), true);
+						$this->getLevel()->setBlock($this, Block::get($this->id, $decay), true, true);
 						$this->getLevel()->scheduleDelayedBlockUpdate($this, $this->tickRate());
 					}
 				}elseif($flag){
@@ -254,19 +254,13 @@ abstract class Liquid extends Transparent{
 
 			$bottomBlock = $this->level->getBlock($this->temporalVector->setComponents($this->x, $this->y - 1, $this->z));
 
-			if($bottomBlock->canBeFlowedInto() or $bottomBlock instanceof Liquid){
-				if($this instanceof FlowingLava and $bottomBlock instanceof FlowingWater){
-					$this->getLevel()->setBlock($bottomBlock, Block::get(Item::STONE), true);
-					return;
-				}
+			if($this instanceof FlowingLava and $bottomBlock instanceof FlowingWater){
+				$this->getLevel()->setBlock($bottomBlock, Block::get(Block::STONE), true, true);
 
-				if($decay >= 8){
-					$this->getLevel()->setBlock($bottomBlock, Block::get($this->id, $decay), true);
-					$this->getLevel()->scheduleDelayedBlockUpdate($bottomBlock, $this->tickRate());
-				}else{
-					$this->getLevel()->setBlock($bottomBlock, Block::get($this->id, $decay + 8), true);
-					$this->getLevel()->scheduleDelayedBlockUpdate($bottomBlock, $this->tickRate());
-				}
+			}elseif($bottomBlock->canBeFlowedInto() or ($bottomBlock instanceof Liquid and ($bottomBlock->getDamage() & 0x07) !== 0)){
+				$this->getLevel()->setBlock($bottomBlock, Block::get($this->id, $decay | 0x08), true, false);
+				$this->getLevel()->scheduleDelayedBlockUpdate($bottomBlock, $this->tickRate());
+
 			}elseif($decay >= 0 and ($decay === 0 or !$bottomBlock->canBeFlowedInto())){
 				$flags = $this->getOptimalFlowDirections();
 
@@ -309,7 +303,7 @@ abstract class Liquid extends Transparent{
 				$this->getLevel()->useBreakOn($block);
 			}
 
-			$this->getLevel()->setBlock($block, Block::get($this->getId(), $newFlowDecay), true);
+			$this->getLevel()->setBlock($block, Block::get($this->getId(), $newFlowDecay), true, false);
 			$this->getLevel()->scheduleDelayedBlockUpdate($block, $this->tickRate());
 		}
 	}
@@ -438,9 +432,9 @@ abstract class Liquid extends Transparent{
 
 			if($colliding){
 				if($this->getDamage() === 0){
-					$this->getLevel()->setBlock($this, Block::get(Item::OBSIDIAN), true);
+					$this->getLevel()->setBlock($this, Block::get(Item::OBSIDIAN), true, true);
 				}elseif($this->getDamage() <= 4){
-					$this->getLevel()->setBlock($this, Block::get(Item::COBBLESTONE), true);
+					$this->getLevel()->setBlock($this, Block::get(Item::COBBLESTONE), true, true);
 				}
 			}
 		}

xpocketmc/XPocketMP-server

src/pocketmine/block/Liquid.php
@@ -241,9 +241,9 @@ abstract class Liquid extends Transparent{
 				if($k !== $decay){
 					$decay = $k;
 					if($decay < 0){
-						$this->getLevel()->setBlock($this, new Air(), true);
+						$this->getLevel()->setBlock($this, new Air(), true, true);
 					}else{
-						$this->getLevel()->setBlock($this, Block::get($this->id, $decay), true);
+						$this->getLevel()->setBlock($this, Block::get($this->id, $decay), true, true);
 						$this->getLevel()->scheduleDelayedBlockUpdate($this, $this->tickRate());
 					}
 				}elseif($flag){
@@ -256,19 +256,13 @@ abstract class Liquid extends Transparent{
 
 			$bottomBlock = $this->level->getBlock($this->temporalVector->setComponents($this->x, $this->y - 1, $this->z));
 
-			if($bottomBlock->canBeFlowedInto() or $bottomBlock instanceof Liquid){
-				if($this instanceof Lava and $bottomBlock instanceof Water){
-					$this->getLevel()->setBlock($bottomBlock, Block::get(Item::STONE), true);
-					return;
-				}
+			if($this instanceof Lava and $bottomBlock instanceof Water){
+				$this->getLevel()->setBlock($bottomBlock, Block::get(Block::STONE), true, true);
 
-				if($decay >= 8){
-					$this->getLevel()->setBlock($bottomBlock, Block::get($this->id, $decay), true);
-					$this->getLevel()->scheduleDelayedBlockUpdate($bottomBlock, $this->tickRate());
-				}else{
-					$this->getLevel()->setBlock($bottomBlock, Block::get($this->id, $decay + 8), true);
-					$this->getLevel()->scheduleDelayedBlockUpdate($bottomBlock, $this->tickRate());
-				}
+			}elseif($bottomBlock->canBeFlowedInto() or ($bottomBlock instanceof Liquid and ($bottomBlock->getDamage() & 0x07) !== 0)){
+				$this->getLevel()->setBlock($bottomBlock, Block::get($this->id, $decay | 0x08), true, false);
+				$this->getLevel()->scheduleDelayedBlockUpdate($bottomBlock, $this->tickRate());
+
 			}elseif($decay >= 0 and ($decay === 0 or !$bottomBlock->canBeFlowedInto())){
 				$flags = $this->getOptimalFlowDirections();
 
@@ -311,7 +305,7 @@ abstract class Liquid extends Transparent{
 				$this->getLevel()->useBreakOn($block);
 			}
 
-			$this->getLevel()->setBlock($block, Block::get($this->getId(), $newFlowDecay), true);
+			$this->getLevel()->setBlock($block, Block::get($this->getId(), $newFlowDecay), true, false);
 			$this->getLevel()->scheduleDelayedBlockUpdate($block, $this->tickRate());
 		}
 	}
@@ -440,9 +434,9 @@ abstract class Liquid extends Transparent{
 
 			if($colliding){
 				if($this->getDamage() === 0){
-					$this->getLevel()->setBlock($this, Block::get(Item::OBSIDIAN), true);
+					$this->getLevel()->setBlock($this, Block::get(Item::OBSIDIAN), true, true);
 				}elseif($this->getDamage() <= 4){
-					$this->getLevel()->setBlock($this, Block::get(Item::COBBLESTONE), true);
+					$this->getLevel()->setBlock($this, Block::get(Item::COBBLESTONE), true, true);
 				}
 			}
 		}