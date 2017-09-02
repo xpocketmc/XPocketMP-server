xpocketmc/XPocketMP-server

commit 8aa3915f20fc0e5b10f1aede5b3ae046c1f3322d

setText now nullable instead of overwriting with empty lines (#1204)

src/pocketmine/tile/Sign.php
@@ -54,11 +54,30 @@ class Sign extends Spawnable{
 		unset($this->namedtag->Creator);
 	}
 
+	/**
+	 * Changes contents of the specific lines to the string provided. 
+	 * Leaves contents of the specifc lines as is if null is provided.
+	 *
+	 * @param null|string $line1
+	 * @param null|string $line2
+	 * @param null|string $line3
+	 * @param null|string $line4
+	 *
+	 * @return bool
+	 */
 	public function setText($line1 = "", $line2 = "", $line3 = "", $line4 = ""){
-		$this->namedtag->Text1->setValue($line1);
-		$this->namedtag->Text2->setValue($line2);
-		$this->namedtag->Text3->setValue($line3);
-		$this->namedtag->Text4->setValue($line4);
+		if($line1 !== null){
+			$this->namedtag->Text1->setValue("Text1", $line1);
+		}
+		if($line2 !== null){
+			$this->namedtag->Text2->setValue("Text2", $line2);
+		}
+		if($line3 !== null){
+			$this->namedtag->Text3->setValue("Text3", $line3);
+		}
+		if($line4 !== null){
+			$this->namedtag->Text4->setValue("Text4", $line4);
+		}
 		$this->onChanged();
 	}
 

commit 1d13fec37a4503310b6155e0e90821aafc042943

Fixed sign bug introduced in #1204 rebase, close #1346

src/pocketmine/tile/Sign.php
@@ -62,21 +62,19 @@ class Sign extends Spawnable{
 	 * @param null|string $line2
 	 * @param null|string $line3
 	 * @param null|string $line4
-	 *
-	 * @return bool
 	 */
 	public function setText($line1 = "", $line2 = "", $line3 = "", $line4 = ""){
 		if($line1 !== null){
-			$this->namedtag->Text1->setValue("Text1", $line1);
+			$this->namedtag->Text1->setValue($line1);
 		}
 		if($line2 !== null){
-			$this->namedtag->Text2->setValue("Text2", $line2);
+			$this->namedtag->Text2->setValue($line2);
 		}
 		if($line3 !== null){
-			$this->namedtag->Text3->setValue("Text3", $line3);
+			$this->namedtag->Text3->setValue($line3);
 		}
 		if($line4 !== null){
-			$this->namedtag->Text4->setValue("Text4", $line4);
+			$this->namedtag->Text4->setValue($line4);
 		}
 		$this->onChanged();
 	}