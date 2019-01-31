xpocketmc/XPocketMP-server

commit 717e8b66ea9bf01ff05dfcef1c0f378d9087aa61

Level: fixed setChunk() leaking memory when not copying tiles/entities closes #2719

src/pocketmine/level/Level.php
@@ -2489,6 +2489,13 @@ class Level implements ChunkManager, Metadatable{
 		$oldChunk = $this->getChunk($chunkX, $chunkZ, false);
 		if($oldChunk !== null and $oldChunk !== $chunk){
 			if($deleteEntitiesAndTiles){
+				$players = $this->getChunkPlayers($chunkX, $chunkZ);
+				foreach($players as $player){
+					$chunk->addEntity($player);
+					$oldChunk->removeEntity($player);
+					$player->chunk = $chunk;
+				}
+				//TODO: this causes chunkloaders to receive false "unloaded" notifications
 				$this->unloadChunk($chunkX, $chunkZ, false, false);
 			}else{
 				foreach($oldChunk->getEntities() as $entity){

commit 07ac3aee811781a6c0d0f05f7ead97345e849479

Level: fixed setChunk() bug introduced by 717e8b66ea9bf01ff05dfcef1c0f378d9087aa61 Anyone who is using getChunkPlayers() should probably check that their code is actually doing what they think it's doing.

src/pocketmine/level/Level.php
@@ -616,7 +616,10 @@ class Level implements ChunkManager, Metadatable{
 	}
 
 	/**
-	 * Gets the players being used in a specific chunk
+	 * @deprecated WARNING: This function has a misleading name. Contrary to what the name might imply, this function
+	 * DOES NOT return players who are IN a chunk, rather, it returns players who can SEE the chunk.
+	 *
+	 * Returns a list of players who have the target chunk within their view distance.
 	 *
 	 * @param int $chunkX
 	 * @param int $chunkZ
@@ -2490,8 +2493,10 @@ class Level implements ChunkManager, Metadatable{
 		$oldChunk = $this->getChunk($chunkX, $chunkZ, false);
 		if($oldChunk !== null and $oldChunk !== $chunk){
 			if($deleteEntitiesAndTiles){
-				$players = $this->getChunkPlayers($chunkX, $chunkZ);
-				foreach($players as $player){
+				foreach($oldChunk->getEntities() as $player){
+					if(!($player instanceof Player)){
+						continue;
+					}
 					$chunk->addEntity($player);
 					$oldChunk->removeEntity($player);
 					$player->chunk = $chunk;