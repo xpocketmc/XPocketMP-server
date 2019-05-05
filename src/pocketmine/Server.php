xpocketmc/XPocketMP-server

commit cb31203ec72964aeecdfbb9b4c843fa055496337

Log a debug message when not sending crash due to folder plugin

src/pocketmine/Server.php
@@ -2280,7 +2280,7 @@ class Server{
 				if(is_string($plugin)){
 					$p = $this->pluginManager->getPlugin($plugin);
 					if($p instanceof Plugin and !($p->getPluginLoader() instanceof PharPluginLoader)){
-						$report = false;
+						$this->logger->debug("Not sending crashdump due to caused by non-phar plugin");
 					}
 				}
 

commit 838130149cc590e2d4cabd92aacd688fe8170030

Server: fixed crash reporting bug introduced by cb31203ec72964aeecdfbb9b4c843fa055496337

src/pocketmine/Server.php
@@ -2271,6 +2271,7 @@ class Server{
 					$p = $this->pluginManager->getPlugin($plugin);
 					if($p instanceof Plugin and !($p->getPluginLoader() instanceof PharPluginLoader)){
 						$this->logger->debug("Not sending crashdump due to caused by non-phar plugin");
+						$report = false;
 					}
 				}
 