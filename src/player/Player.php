xpocketmc/XPocketMP-server

commit f21bc3d54674458b30bfb68b066fecefbf21090e

Player: avoid another empty() usage

src/player/Player.php
@@ -1958,8 +1958,8 @@ class Player extends Human implements CommandSender, ChunkLoader, ChunkListener,
 
 		$ev = new PlayerQuitEvent($this, $quitMessage ?? $this->getLeaveMessage(), $reason);
 		$ev->call();
-		if(!empty($ev->getQuitMessage())){
-			$this->server->broadcastMessage($ev->getQuitMessage());
+		if($quitMessage != ""){
+			$this->server->broadcastMessage($quitMessage);
 		}
 		$this->save();
 

commit 834f5b26c7cc2182d06ca3bba66d7945a8a91d22

Player: fix regression in disconnect caused by f21bc3d54674458b30bfb68b066fecefbf21090e

src/player/Player.php
@@ -1958,7 +1958,7 @@ class Player extends Human implements CommandSender, ChunkLoader, ChunkListener,
 
 		$ev = new PlayerQuitEvent($this, $quitMessage ?? $this->getLeaveMessage(), $reason);
 		$ev->call();
-		if($quitMessage != ""){
+		if(($quitMessage = $ev->getQuitMessage()) != ""){
 			$this->server->broadcastMessage($quitMessage);
 		}
 		$this->save();