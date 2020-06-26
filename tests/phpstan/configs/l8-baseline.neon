xpocketmc/XPocketMP-server

commit 4802d6a3763907c794086594640b678b0ce3521d

CommandSender: export getLanguage() this currently serves as a proxy to the server main language, but it can be used by third party implementations to choose a non-Server language.

src/Server.php
@@ -1290,7 +1290,7 @@ class Server{
 			return true;
 		}
 
-		$sender->sendMessage($this->getLanguage()->translateString(TextFormat::RED . "%commands.generic.notFound"));
+		$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.notFound"));
 
 		return false;
 	}

src/command/Command.php
@@ -108,7 +108,7 @@ abstract class Command{
 		}
 
 		if($this->permissionMessage === null){
-			$target->sendMessage($target->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+			$target->sendMessage($target->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 		}elseif($this->permissionMessage !== ""){
 			$target->sendMessage(str_replace("<permission>", $this->permission, $this->permissionMessage));
 		}
@@ -229,7 +229,7 @@ abstract class Command{
 	public static function broadcastCommandMessage(CommandSender $source, $message, bool $sendToSource = true) : void{
 		$users = PermissionManager::getInstance()->getPermissionSubscriptions(Server::BROADCAST_CHANNEL_ADMINISTRATIVE);
 		if($message instanceof TranslationContainer){
-			$formatted = "[" . $source->getName() . ": " . ($source->getServer()->getLanguage()->get($message->getText()) !== $message->getText() ? "%" : "") . $message->getText() . "]";
+			$formatted = "[" . $source->getName() . ": " . ($source->getLanguage()->get($message->getText()) !== $message->getText() ? "%" : "") . $message->getText() . "]";
 
 			$result = new TranslationContainer($formatted, $message->getParameters());
 			$colored = new TranslationContainer(TextFormat::GRAY . TextFormat::ITALIC . $formatted, $message->getParameters());

src/command/CommandSender.php
@@ -23,11 +23,14 @@ declare(strict_types=1);
 
 namespace pocketmine\command;
 
+use pocketmine\lang\Language;
 use pocketmine\lang\TranslationContainer;
 use pocketmine\permission\Permissible;
 use pocketmine\Server;
 
 interface CommandSender extends Permissible{
+
+	public function getLanguage() : Language;
 
 	/**
 	 * @param TranslationContainer|string $message

src/command/ConsoleCommandSender.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 
 namespace pocketmine\command;
 
+use pocketmine\lang\Language;
 use pocketmine\lang\TranslationContainer;
 use pocketmine\permission\PermissibleBase;
 use pocketmine\permission\PermissibleDelegateTrait;
@@ -48,15 +49,19 @@ class ConsoleCommandSender implements CommandSender{
 		return $this->server;
 	}
 
+	public function getLanguage() : Language{
+		return $this->server->getLanguage();
+	}
+
 	/**
 	 * @param TranslationContainer|string $message
 	 */
 	public function sendMessage($message) : void{
 		$server = $this->getServer();
 		if($message instanceof TranslationContainer){
-			$message = $server->getLanguage()->translate($message);
+			$message = $this->getLanguage()->translate($message);
 		}else{
-			$message = $server->getLanguage()->translateString($message);
+			$message = $this->getLanguage()->translateString($message);
 		}
 
 		foreach(explode("\n", trim($message)) as $line){

src/command/SimpleCommandMap.php
@@ -243,7 +243,7 @@ class SimpleCommandMap implements CommandMap{
 		try{
 			$target->execute($sender, $sentCommandLabel, $args);
 		}catch(InvalidCommandSyntaxException $e){
-			$sender->sendMessage($this->server->getLanguage()->translateString("commands.generic.usage", [$target->getUsage()]));
+			$sender->sendMessage($sender->getLanguage()->translateString("commands.generic.usage", [$target->getUsage()]));
 		}finally{
 			$target->timings->stopTiming();
 		}

src/command/defaults/KillCommand.php
@@ -55,7 +55,7 @@ class KillCommand extends VanillaCommand{
 
 		if(count($args) === 1){
 			if(!$sender->hasPermission("pocketmine.command.kill.other")){
-				$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+				$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 				return true;
 			}
@@ -74,7 +74,7 @@ class KillCommand extends VanillaCommand{
 
 		if($sender instanceof Player){
 			if(!$sender->hasPermission("pocketmine.command.kill.self")){
-				$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+				$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 				return true;
 			}

src/command/defaults/TimeCommand.php
@@ -50,7 +50,7 @@ class TimeCommand extends VanillaCommand{
 
 		if($args[0] === "start"){
 			if(!$sender->hasPermission("pocketmine.command.time.start")){
-				$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+				$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 				return true;
 			}
@@ -61,7 +61,7 @@ class TimeCommand extends VanillaCommand{
 			return true;
 		}elseif($args[0] === "stop"){
 			if(!$sender->hasPermission("pocketmine.command.time.stop")){
-				$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+				$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 				return true;
 			}
@@ -72,7 +72,7 @@ class TimeCommand extends VanillaCommand{
 			return true;
 		}elseif($args[0] === "query"){
 			if(!$sender->hasPermission("pocketmine.command.time.query")){
-				$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+				$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 				return true;
 			}
@@ -81,7 +81,7 @@ class TimeCommand extends VanillaCommand{
 			}else{
 				$world = $sender->getServer()->getWorldManager()->getDefaultWorld();
 			}
-			$sender->sendMessage($sender->getServer()->getLanguage()->translateString("commands.time.query", [$world->getTime()]));
+			$sender->sendMessage($sender->getLanguage()->translateString("commands.time.query", [$world->getTime()]));
 			return true;
 		}
 
@@ -91,7 +91,7 @@ class TimeCommand extends VanillaCommand{
 
 		if($args[0] === "set"){
 			if(!$sender->hasPermission("pocketmine.command.time.set")){
-				$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+				$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 				return true;
 			}
@@ -126,7 +126,7 @@ class TimeCommand extends VanillaCommand{
 			Command::broadcastCommandMessage($sender, new TranslationContainer("commands.time.set", [$value]));
 		}elseif($args[0] === "add"){
 			if(!$sender->hasPermission("pocketmine.command.time.add")){
-				$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+				$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 				return true;
 			}

src/command/defaults/WhitelistCommand.php
@@ -117,7 +117,7 @@ class WhitelistCommand extends VanillaCommand{
 			"off" => "disable"
 		];
 		if(!$sender->hasPermission("pocketmine.command.whitelist." . ($map[$subcommand] ?? $subcommand))){
-			$sender->sendMessage($sender->getServer()->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
+			$sender->sendMessage($sender->getLanguage()->translateString(TextFormat::RED . "%commands.generic.permission"));
 
 			return true;
 		}

commit fbc829479db23f62934016c15e8c3a02d4452d5e

phpstan: ignore a new error caused by 4802d6a3763907c794086594640b678b0ce3521d

tests/phpstan/configs/l8-baseline.neon
@@ -416,6 +416,11 @@ parameters:
 			path: ../../../src/network/mcpe/NetworkSession.php
 
 		-
+			message: "#^Cannot call method getLanguage\\(\\) on pocketmine\\\\player\\\\Player\\|null\\.$#"
+			count: 1
+			path: ../../../src/network/mcpe/NetworkSession.php
+
+		-
 			message: "#^Cannot call method getLocation\\(\\) on pocketmine\\\\player\\\\Player\\|null\\.$#"
 			count: 2
 			path: ../../../src/network/mcpe/NetworkSession.php