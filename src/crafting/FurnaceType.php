e868547d28c88caf98ee92009a82ff6932554aa6
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-01 06:18:42 +0700

    Assemble 1.21.40

--- src/crafting/FurnaceType.php
+++ src/crafting/FurnaceType.php
@@ -25,7 +25,7 @@ namespace pocketmine\crafting;
 
 use pocketmine\utils\LegacyEnumShimTrait;
 use pocketmine\world\sound\BlastFurnaceSound;
-use pocketmine\world\sound\CampFireSound;
+use pocketmine\world\sound\CampfireSound;
 use pocketmine\world\sound\FurnaceSound;
 use pocketmine\world\sound\SmokerSound;
 use pocketmine\world\sound\Sound;
@@ -63,7 +63,8 @@ enum FurnaceType{
 			self::FURNACE => [200, new FurnaceSound()],
 			self::BLAST_FURNACE => [100, new BlastFurnaceSound()],
 			self::SMOKER => [100, new SmokerSound()],
-			self::CAMPFIRE, self::SOUL_CAMPFIRE => [100, new CampFireSound()]
+			self::CAMPFIRE => [100, new CampfireSound()],
+			self::SOUL_CAMPFIRE => [100, new CampfireSound()]
 		};
 	}
 
