7682ea70ae2a70fd98431577440f3d3f7c29fca1
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-08 18:19:29 +0700

    Update BlockObjectToStateSerializer.php

--- src/data/bedrock/block/convert/BlockObjectToStateSerializer.php
+++ src/data/bedrock/block/convert/BlockObjectToStateSerializer.php
@@ -775,6 +775,7 @@ final class BlockObjectToStateSerializer implements BlockStateSerializer{
 			Ids::DARK_OAK_SAPLING => Blocks::DARK_OAK_SAPLING(),
 			Ids::JUNGLE_SAPLING => Blocks::JUNGLE_SAPLING(),
 			Ids::OAK_SAPLING => Blocks::OAK_SAPLING(),
+			Ids::PALE_OAK_SAPLING => Blocks::PALE_OAK_SAPLING(),
 			Ids::SPRUCE_SAPLING => Blocks::SPRUCE_SAPLING(),
 		] as $id => $block){
 			$this->map($block, fn(Sapling $block) => Helper::encodeSapling($block, new Writer($id)));
