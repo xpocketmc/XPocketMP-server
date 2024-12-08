b93c4e452665ca0087f6f9783292f6a2dfef4f2c
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-08 18:07:53 +0700

    Update WoodLikeBlockIdHelper.php

--- src/block/WoodLikeBlockIdHelper.php
+++ src/block/WoodLikeBlockIdHelper.php
@@ -150,6 +150,7 @@ final class WoodLikeBlockIdHelper{
 			SaplingType::JUNGLE => Ids::JUNGLE_SAPLING,
 			SaplingType::ACACIA => Ids::ACACIA_SAPLING,
 			SaplingType::DARK_OAK => Ids::DARK_OAK_SAPLING,
+			SaplingType::PALE_OAK => Ids::PALE_OAK,
 		});
 	}
 
