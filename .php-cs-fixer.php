0bcf6bc11094157599a48cddfaaf97ff39c7879d
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-12-12 17:53:39 +0700

    Update license header

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -46,19 +46,19 @@ return (new PhpCsFixer\Config)
             'comment_type' => 'comment',
             'header' => <<<BODY
 
- ____            _        _   __  __ _                  __  __ ____
-|  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
-| |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
-|  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
-|_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ __  ______            _        _   __  __ ____  
+ \ \/ /  _ \ ___   ___| | _____| |_|  \/  |  _ \ 
+  \  /| |_) / _ \ / __| |/ / _ \ __| |\/| | |_) |
+  /  \|  __/ (_) | (__|   <  __/ |_| |  | |  __/ 
+ /_/\_\_|   \___/ \___|_|\_\___|\__|_|  |_|_|    
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the MIT License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-@author PocketMine Team
-@link http://www.pocketmine.net/
+@author ClousClouds Team
+@link https://xpocketmc.xyz/
 
 
 BODY,
