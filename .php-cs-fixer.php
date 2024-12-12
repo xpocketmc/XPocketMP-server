b934b0e5a64890596cf478b0135e46490f78d52b
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-12-12 17:46:29 +0700

    Change License Header

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -53,7 +53,7 @@ return (new PhpCsFixer\Config)
 |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
+it under the terms of the MIT License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
