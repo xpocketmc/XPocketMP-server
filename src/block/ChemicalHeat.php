e06fc7283de862f7929ff4b91cdc583609e70b1f
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-12-19 16:38:54 +0700

    Use sound dependencies (#99)
    Co-authored-by: xpocketmp[bot] <190384261+xpocketmp[bot]@users.noreply.github.com>

--- src/block/ChemicalHeat.php
+++ src/block/ChemicalHeat.php
@@ -12,7 +12,7 @@
  * it under the terms of the MIT License as published by
  * the Free Software Foundation
  * The files in XPocketMP are mostly from PocketMine-MP.
- *
+ * Developed: ClousClouds, PMMP Team
  * @author ClousClouds Team
  * @link https://xpocketmc.xyz/
  *
