a4fd81cce7cf4e2b9a37c585619516b1b6280fcf
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-26 18:20:53 +0700

    Update MossBlock.php

--- src/block/MossBlock.php
+++ src/block/MossBlock.php
@@ -9,28 +9,28 @@ use pocketmine\item\Item;
 use pocketmine\item\ItemTypeIds;
 use pocketmine\world\World;
 
-class MossBlock extends Solid {
+class MossBlock extends Opaque{
 
     protected float $hardness = 0.1;
     protected float $blastResistance = 1.0;
 
-    public function getToolType(): ToolType {
-        return ToolType::SHOVEL();
+    public function getToolType(): ToolType{
+        return BlockToolType::SHOVEL();
     }
 
-    public function onRandomTick(): void {
+    public function onRandomTick(): void{
         $world = $this->position->getWorld();
-        foreach ($this->getSpreadableNeighbors() as $neighbor) {
+        foreach($this->getSpreadableNeighbors() as $neighbor){
             $block = $world->getBlockAt($neighbor);
-            if ($block instanceof Dirt && !$block->isCovered()) {
+            if($block instanceof Dirt && !$block->isCovered()){
                 $world->setBlockAt($neighbor, new MossBlock());
             }
         }
     }
 
-    private function getSpreadableNeighbors(): array {
+    private function getSpreadableNeighbors(): array{
         $pos = $this->position;
-        return [
+        return[
             $pos->north(),
             $pos->south(),
             $pos->east(),
