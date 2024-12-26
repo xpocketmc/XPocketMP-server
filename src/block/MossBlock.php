947c9798584fe3b9ca64f170153e971f8b2adae7
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-26 18:09:33 +0700

    Create MossBlock.php

---
+++ src/block/MossBlock.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\block;
+
+use pocketmine\block\utils\ToolType;
+use pocketmine\item\Item;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\world\World;
+
+class MossBlock extends Solid {
+
+    protected float $hardness = 0.1;
+    protected float $blastResistance = 1.0;
+
+    public function getToolType(): ToolType {
+        return ToolType::SHOVEL();
+    }
+
+    public function onRandomTick(): void {
+        $world = $this->position->getWorld();
+        foreach ($this->getSpreadableNeighbors() as $neighbor) {
+            $block = $world->getBlockAt($neighbor);
+            if ($block instanceof Dirt && !$block->isCovered()) {
+                $world->setBlockAt($neighbor, new MossBlock());
+            }
+        }
+    }
+
+    private function getSpreadableNeighbors(): array {
+        $pos = $this->position;
+        return [
+            $pos->north(),
+            $pos->south(),
+            $pos->east(),
+            $pos->west(),
+            $pos->up(),
+        ];
+    }
+}
