d7244aaa6db0ca32a2d2fed208e1e3bb93702e6c
NurAzli <144025939+NurAzli@users.noreply.github.com>
2025-01-01 11:05:35 +0700

    Create EatGrassAnimation.php

---
+++ src/entity/animation/EatGrassAnimation.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ *
+ *  __  ______            _        _   __  __ ____
+ *  \ \/ /  _ \ ___   ___| | _____| |_|  \/  |  _ \
+ *   \  /| |_) / _ \ / __| |/ / _ \ __| |\/| | |_) |
+ *   /  \|  __/ (_) | (__|   <  __/ |_| |  | |  __/
+ *  /_/\_\_|   \___/ \___|_|\_\___|\__|_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License as published by
+ * the Free Software Foundation
+ * The files in XPocketMP are mostly from PocketMine-MP.
+ * Developed by ClousClouds, PMMP Team
+ *
+ * @author ClousClouds Team
+ * @link https://xpocketmc.xyz/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace pocketmine\entity\animation;
+
+use pocketmine\entity\Living;
+use pocketmine\network\mcpe\protocol\ActorEventPacket;
+use pocketmine\network\mcpe\protocol\types\ActorEvent;
+
+final class EatGrassAnimation implements Animation{
+
+	public function __construct(private Living $entity){}
+
+	public function encode() : array{
+		return [
+			ActorEventPacket::create($this->entity->getId(), ActorEvent::EAT_GRASS_ANIMATION, 0)
+		];
+	}
+}
