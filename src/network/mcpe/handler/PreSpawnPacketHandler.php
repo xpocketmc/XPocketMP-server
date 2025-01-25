6cb3b0c54083053eedbf0224467c5750bcec6d55
NurAzli <144025939+NurAzli@users.noreply.github.com>
2025-01-25 17:14:17 +0700

    Update PreSpawnPacketHandler.php

--- src/network/mcpe/handler/PreSpawnPacketHandler.php
+++ src/network/mcpe/handler/PreSpawnPacketHandler.php
@@ -24,6 +24,8 @@ declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\handler;
 
+use pocketmine\entity\attribute\Attribute;
+use pocketmine\entity\attribute\AttributeMap;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\cache\CraftingDataCache;
 use pocketmine\network\mcpe\cache\StaticPacketCache;
