b26c2a60c20bfb2569f4048ab84410ae51df69dd
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-07 15:02:52 +0700

    Update PluginManager.php

--- src/plugin/PluginManager.php
+++ src/plugin/PluginManager.php
@@ -82,8 +82,6 @@ class PluginManager{
 	 */
 	protected array $enabledPlugins = [];
 
-	private Plugin $plugin;
-
 	/** @var array<string, array<string, true>> */
 	private array $pluginDependents = [];
 
@@ -108,12 +106,6 @@ class PluginManager{
 			}elseif(!is_dir($this->pluginDataDirectory)){
 				throw new \RuntimeException("Plugin data path $this->pluginDataDirectory exists and is not a directory");
 			}
-		}
-		$this->plugin = $plugin
-		try {
-    $this->plugin->onLoad();
-} catch (\Throwable $e) {
-    CrashHandler::getInstance()->handlePluginCrash($plugin, $e);
 		}
 	}
 
