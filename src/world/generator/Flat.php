b48081b8aacc49b876e4ffa4066e980b3056d6a4
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-10 18:45:18 +0700

    Update Flat.php

--- src/world/generator/Flat.php
+++ src/world/generator/Flat.php
@@ -1,25 +1,5 @@
 <?php
 
-/*
- *
- *  __  ______            _        _   __  __ ____
- *  \ \/ /  _ \ ___   ___| | _____| |_|  \/  |  _ \
- *   \  /| |_) / _ \ / __| |/ / _ \ __| |\/| | |_) |
- *   /  \|  __/ (_) | (__|   <  __/ |_| |  | |  __/
- *  /_/\_\_|   \___/ \___|_|\_\___|\__|_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the MIT License as published by
- * the Free Software Foundation
- * The files in XPocketMP are mostly from PocketMine-MP.
- * Developed by ClousClouds, PMMP Team
- *
- * @author ClousClouds Team
- * @link https://xpocketmc.xyz/
- *
- *
- */
-
 declare(strict_types=1);
 
 namespace pocketmine\world\generator;
@@ -30,70 +10,70 @@ use pocketmine\world\format\Chunk;
 use pocketmine\world\format\SubChunk;
 use pocketmine\world\generator\object\OreType;
 use pocketmine\world\generator\populator\Ore;
-use pocketmine\world\generator\populator\Populator;
 use function count;
 
-class Flat extends Generator{
-	private Chunk $chunk;
-	/** @var Populator[] */
-	private array $populators = [];
+class Flat extends Generator {
+    private Chunk $chunk;
+    private array $populators = [];
 
-	private FlatGeneratorOptions $options;
+    /**
+     * @throws InvalidGeneratorOptionsException
+     */
+    public function __construct(int $seed, string $preset) {
+        parent::__construct($seed, $preset !== "" ? $preset : "3;bedrock,5xdirt,grass;1;");
 
-	/**
-	 * @throws InvalidGeneratorOptionsException
-	 */
-	public function __construct(int $seed, string $preset){
-		parent::__construct($seed, $preset !== "" ? $preset : "2;bedrock,2xdirt,grass;1;");
-		$this->options = FlatGeneratorOptions::parsePreset($this->preset);
+        $ores = new Ore();
+        $stone = VanillaBlocks::STONE();
+        $ores->setOreTypes([
+            new OreType(VanillaBlocks::COAL_ORE(), $stone, 10, 16, 1, 30),
+            new OreType(VanillaBlocks::IRON_ORE(), $stone, 8, 8, 1, 25),
+            new OreType(VanillaBlocks::REDSTONE_ORE(), $stone, 3, 7, 1, 15),
+            new OreType(VanillaBlocks::LAPIS_LAZULI_ORE(), $stone, 1, 6, 1, 15),
+            new OreType(VanillaBlocks::GOLD_ORE(), $stone, 2, 8, 1, 20),
+            new OreType(VanillaBlocks::DIAMOND_ORE(), $stone, 1, 7, 1, 10)
+        ]);
+        $this->populators[] = $ores;
 
-		if(isset($this->options->getExtraOptions()["decoration"])){
-			$ores = new Ore();
-			$stone = VanillaBlocks::STONE();
-			$ores->setOreTypes([
-				new OreType(VanillaBlocks::COAL_ORE(), $stone, 20, 16, 0, 128),
-				new OreType(VanillaBlocks::IRON_ORE(), $stone, 20, 8, 0, 64),
-				new OreType(VanillaBlocks::REDSTONE_ORE(), $stone, 8, 7, 0, 16),
-				new OreType(VanillaBlocks::LAPIS_LAZULI_ORE(), $stone, 1, 6, 0, 32),
-				new OreType(VanillaBlocks::GOLD_ORE(), $stone, 2, 8, 0, 32),
-				new OreType(VanillaBlocks::DIAMOND_ORE(), $stone, 1, 7, 0, 16),
-				new OreType(VanillaBlocks::DIRT(), $stone, 20, 32, 0, 128),
-				new OreType(VanillaBlocks::GRAVEL(), $stone, 10, 16, 0, 128)
-			]);
-			$this->populators[] = $ores;
-		}
+        $this->generateBaseChunk();
+    }
 
-		$this->generateBaseChunk();
-	}
+    protected function generateBaseChunk(): void {
+        $this->chunk = new Chunk([], false);
 
-	protected function generateBaseChunk() : void{
-		$this->chunk = new Chunk([], false);
+        // Struktur tanah mirip dunia Flat Vanilla
+        $structure = [
+            VanillaBlocks::BEDROCK()->getStateId(),
+            VanillaBlocks::DIRT()->getStateId(),
+            VanillaBlocks::DIRT()->getStateId(),
+            VanillaBlocks::DIRT()->getStateId(),
+            VanillaBlocks::DIRT()->getStateId(),
+            VanillaBlocks::DIRT()->getStateId(),
+            VanillaBlocks::GRASS()->getStateId()
+        ];
 
-		$structure = $this->options->getStructure();
-		$count = count($structure);
-		for($sy = 0; $sy < $count; $sy += SubChunk::EDGE_LENGTH){
-			$subchunk = $this->chunk->getSubChunk($sy >> SubChunk::COORD_BIT_SIZE);
-			for($y = 0; $y < SubChunk::EDGE_LENGTH && isset($structure[$y | $sy]); ++$y){
-				$id = $structure[$y | $sy];
+        $count = count($structure);
+        for ($sy = 0; $sy < $count; $sy += SubChunk::EDGE_LENGTH) {
+            $subchunk = $this->chunk->getSubChunk($sy >> SubChunk::COORD_BIT_SIZE);
+            for ($y = 0; $y < SubChunk::EDGE_LENGTH && isset($structure[$y | $sy]); ++$y) {
+                $id = $structure[$y | $sy];
 
-				for($Z = 0; $Z < SubChunk::EDGE_LENGTH; ++$Z){
-					for($X = 0; $X < SubChunk::EDGE_LENGTH; ++$X){
-						$subchunk->setBlockStateId($X, $y, $Z, $id);
-					}
-				}
-			}
-		}
-	}
+                for ($Z = 0; $Z < SubChunk::EDGE_LENGTH; ++$Z) {
+                    for ($X = 0; $X < SubChunk::EDGE_LENGTH; ++$X) {
+                        $subchunk->setBlockStateId($X, $y, $Z, $id);
+                    }
+                }
+            }
+        }
+    }
 
-	public function generateChunk(ChunkManager $world, int $chunkX, int $chunkZ) : void{
-		$world->setChunk($chunkX, $chunkZ, clone $this->chunk);
-	}
+    public function generateChunk(ChunkManager $world, int $chunkX, int $chunkZ): void {
+        $world->setChunk($chunkX, $chunkZ, clone $this->chunk);
+    }
 
-	public function populateChunk(ChunkManager $world, int $chunkX, int $chunkZ) : void{
-		$this->random->setSeed(0xdeadbeef ^ ($chunkX << 8) ^ $chunkZ ^ $this->seed);
-		foreach($this->populators as $populator){
-			$populator->populate($world, $chunkX, $chunkZ, $this->random);
-		}
-
-	}
+    public function populateChunk(ChunkManager $world, int $chunkX, int $chunkZ): void {
+        $this->random->setSeed($this->seed ^ ($chunkX << 8) ^ $chunkZ);
+        foreach ($this->populators as $populator) {
+            $populator->populate($world, $chunkX, $chunkZ, $this->random);
+        }
+    }
 }
