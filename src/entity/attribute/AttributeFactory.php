a2be6c2cb380f088cccb7176e9f2e192dd4a338e
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-11 18:07:29 +0700

    Update AttributeFactory.php

--- src/entity/attribute/AttributeFactory.php
+++ src/entity/attribute/AttributeFactory.php
@@ -33,23 +33,23 @@ final class AttributeFactory{
 	private array $attributes = [];
 
 	public function __construct(){
-		$this->register(Attribute::ABSORPTION, 0.00, 340282346638528859811704183484516925440.00, 0.00);
-		$this->register(Attribute::SATURATION, 0.00, 20.00, 20.00);
-		$this->register(Attribute::EXHAUSTION, 0.00, 5.00, 0.0, false);
-		$this->register(Attribute::KNOCKBACK_RESISTANCE, 0.00, 1.00, 0.00);
-		$this->register(Attribute::HEALTH, 0.00, 20.00, 20.00);
-		$this->register(Attribute::MOVEMENT_SPEED, 0.00, 340282346638528859811704183484516925440.00, 0.10);
-		$this->register(Attribute::FOLLOW_RANGE, 0.00, 2048.00, 16.00, false);
-		$this->register(Attribute::HUNGER, 0.00, 20.00, 20.00);
-		$this->register(Attribute::ATTACK_DAMAGE, 0.00, 340282346638528859811704183484516925440.00, 1.00, false);
-		$this->register(Attribute::EXPERIENCE_LEVEL, 0.00, 24791.00, 0.00);
-		$this->register(Attribute::EXPERIENCE, 0.00, 1.00, 0.00);
-		$this->register(Attribute::UNDERWATER_MOVEMENT, 0.0, 340282346638528859811704183484516925440.0, 0.02);
-		$this->register(Attribute::LUCK, -1024.0, 1024.0, 0.0);
-		$this->register(Attribute::FALL_DAMAGE, 0.0, 340282346638528859811704183484516925440.0, 1.0);
-		$this->register(Attribute::HORSE_JUMP_STRENGTH, 0.0, 2.0, 0.7);
-		$this->register(Attribute::ZOMBIE_SPAWN_REINFORCEMENTS, 0.0, 1.0, 0.0);
-		$this->register(Attribute::LAVA_MOVEMENT, 0.0, 340282346638528859811704183484516925440.0, 0.02);
+		$this->register(AttributeType::ABSORPTION, 0.00, 340282346638528859811704183484516925440.00, 0.00);
+		$this->register(AttributeType::SATURATION, 0.00, 20.00, 20.00);
+		$this->register(AttributeType::EXHAUSTION, 0.00, 5.00, 0.0, false);
+		$this->register(AttributeType::KNOCKBACK_RESISTANCE, 0.00, 1.00, 0.00);
+		$this->register(AttributeType::HEALTH, 0.00, 20.00, 20.00);
+		$this->register(AttributeType::MOVEMENT_SPEED, 0.00, 340282346638528859811704183484516925440.00, 0.10);
+		$this->register(AttributeType::FOLLOW_RANGE, 0.00, 2048.00, 16.00, false);
+		$this->register(AttributeType::HUNGER, 0.00, 20.00, 20.00);
+		$this->register(AttributeType::ATTACK_DAMAGE, 0.00, 340282346638528859811704183484516925440.00, 1.00, false);
+		$this->register(AttributeType::EXPERIENCE_LEVEL, 0.00, 24791.00, 0.00);
+		$this->register(AttributeType::EXPERIENCE, 0.00, 1.00, 0.00);
+		$this->register(AttributeType::UNDERWATER_MOVEMENT, 0.0, 340282346638528859811704183484516925440.0, 0.02);
+		$this->register(AttributeType::LUCK, -1024.0, 1024.0, 0.0);
+		$this->register(AttributeType::FALL_DAMAGE, 0.0, 340282346638528859811704183484516925440.0, 1.0);
+		$this->register(AttributeType::HORSE_JUMP_STRENGTH, 0.0, 2.0, 0.7);
+		$this->register(AttributeType::ZOMBIE_SPAWN_REINFORCEMENTS, 0.0, 1.0, 0.0);
+		$this->register(AttributeType::LAVA_MOVEMENT, 0.0, 340282346638528859811704183484516925440.0, 0.02);
 	}
 
 	public function get(string $id) : ?Attribute{
