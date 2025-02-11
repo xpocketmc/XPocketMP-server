268f011d244dd8cce979b3062fcb8f76eba5e9c7
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-11 18:04:58 +0700

    Update AttributeMap.php

--- src/entity/attribute/AttributeMap.php
+++ src/entity/attribute/AttributeMap.php
@@ -10,13 +10,12 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the MIT License as published by
- * the Free Software Foundation
+ * the Free Software Foundation.
  * The files in XPocketMP are mostly from PocketMine-MP.
  * Developed by ClousClouds, PMMP Team
  *
  * @author ClousClouds Team
  * @link https://xpocketmc.xyz/
- *
  *
  */
 
@@ -26,31 +25,31 @@ namespace pocketmine\entity\attribute;
 
 use function array_filter;
 
-class AttributeMap{
-	/** @var Attribute[] */
-	private array $attributes = [];
+final class AttributeMap {
+    /** @var array<string, AttributeValue> */
+    private array $attributes = [];
 
-	public function add(Attribute $attribute) : void{
-		$this->attributes[$attribute->getId()] = $attribute;
-	}
+    public function add(AttributeValue $attributeValue): void {
+        $this->attributes[$attributeValue->getAttribute()->getId()] = $attributeValue;
+    }
 
-	public function get(string $id) : ?Attribute{
-		return $this->attributes[$id] ?? null;
-	}
+    public function get(string $id): ?AttributeValue {
+        return $this->attributes[$id] ?? null;
+    }
 
-	/**
-	 * @return Attribute[]
-	 */
-	public function getAll() : array{
-		return $this->attributes;
-	}
+    /**
+     * @return AttributeValue[]
+     */
+    public function getAll(): array {
+        return $this->attributes;
+    }
 
-	/**
-	 * @return Attribute[]
-	 */
-	public function needSend() : array{
-		return array_filter($this->attributes, function(Attribute $attribute) : bool{
-			return $attribute->isSyncable() && $attribute->isDesynchronized();
-		});
-	}
+    /**
+     * @return AttributeValue[]
+     */
+    public function needSend(): array {
+        return array_filter($this->attributes, static fn(AttributeValue $attributeValue): bool =>
+            $attributeValue->getAttribute()->isSyncable() && $attributeValue->isDesynchronized()
+        );
+    }
 }
