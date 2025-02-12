d1e210c914d36a1eab5f32040f4519fea0e0403b
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-12 20:48:04 +0700

    Update CreativeInventory.php

--- src/inventory/CreativeInventory.php
+++ src/inventory/CreativeInventory.php
@@ -25,40 +25,65 @@ declare(strict_types=1);
 namespace pocketmine\inventory;
 
 use pocketmine\crafting\CraftingManagerFromDataHelper;
-use pocketmine\crafting\json\ItemStackData;
 use pocketmine\data\bedrock\BedrockDataFiles;
+use pocketmine\inventory\data\CreativeGroupedItem;
+use pocketmine\inventory\data\CreativeItemGroup;
+use pocketmine\inventory\json\CreativeGroupData;
+use pocketmine\inventory\json\CreativeItemData;
 use pocketmine\item\Item;
 use pocketmine\utils\DestructorCallbackTrait;
+use pocketmine\utils\Filesystem;
 use pocketmine\utils\ObjectSet;
 use pocketmine\utils\SingletonTrait;
 use pocketmine\utils\Utils;
+use function array_map;
+use function count;
+use function json_decode;
 
 final class CreativeInventory{
 	use SingletonTrait;
 	use DestructorCallbackTrait;
 
 	/**
-	 * @var Item[]
-	 * @phpstan-var array<int, Item>
+	 * @var CreativeItemGroup[]
+	 * @phpstan-var array<int, CreativeItemGroup>
 	 */
-	private array $creative = [];
+	private array $groups = [];
+
+	/**
+	 * @var CreativeGroupedItem[]
+	 * @phpstan-var array<int, CreativeGroupedItem>
+	 */
+	private array $items = [];
 
 	/** @phpstan-var ObjectSet<\Closure() : void> */
 	private ObjectSet $contentChangedCallbacks;
 
 	private function __construct(){
 		$this->contentChangedCallbacks = new ObjectSet();
-		$creativeItems = CraftingManagerFromDataHelper::loadJsonArrayOfObjectsFile(
-			BedrockDataFiles::CREATIVEITEMS_JSON,
-			ItemStackData::class
-		);
-		foreach($creativeItems as $data){
-			$item = CraftingManagerFromDataHelper::deserializeItemStack($data);
+
+		$creativeData = json_decode(Filesystem::fileGetContents(BedrockDataFiles::CREATIVEITEMS_JSON));
+
+		$mapper = new \JsonMapper();
+		$mapper->bStrictObjectTypeChecking = true;
+		$mapper->bExceptionOnUndefinedProperty = true;
+		$mapper->bExceptionOnMissingData = true;
+
+		$groups = CraftingManagerFromDataHelper::loadJsonObjectListIntoModel($mapper, CreativeGroupData::class, $creativeData->groups);
+		$items = CraftingManagerFromDataHelper::loadJsonObjectListIntoModel($mapper, CreativeItemData::class, $creativeData->items);
+
+		foreach($groups as $data){
+			$icon = $data->icon === null ? null : CraftingManagerFromDataHelper::deserializeItemStack($data->icon); //todo: discuss what we do if the icon is unknown
+			$this->addGroup($data->category_id, $data->category_name, $icon);
+		}
+
+		foreach($items as $data){
+			$item = CraftingManagerFromDataHelper::deserializeItemStack($data->item);
 			if($item === null){
 				//unknown item
 				continue;
 			}
-			$this->add($item);
+			$this->add($item, $data->group_id);
 		}
 	}
 
@@ -67,7 +92,8 @@ final class CreativeInventory{
 	 * Note: Players who are already online when this is called will not see this change.
 	 */
 	public function clear() : void{
-		$this->creative = [];
+		$this->groups = [];
+		$this->items = [];
 		$this->onContentChange();
 	}
 
@@ -76,16 +102,32 @@ final class CreativeInventory{
 	 * @phpstan-return array<int, Item>
 	 */
 	public function getAll() : array{
-		return Utils::cloneObjectArray($this->creative);
+		return Utils::cloneObjectArray(array_map(static fn(CreativeGroupedItem $d) => $d->item, $this->items));
+	}
+
+	/**
+	 * @return CreativeGroupedItem[]
+	 * @phpstan-return array<int, CreativeGroupedItem>
+	 */
+	public function getGroupedItems() : array{
+		return Utils::cloneObjectArray($this->items);
+	}
+
+	/**
+	 * @return CreativeItemGroup[]
+	 * @phpstan-return array<int, CreativeItemGroup>
+	 */
+	public function getGroups() : array{
+		return Utils::cloneObjectArray($this->groups);
 	}
 
 	public function getItem(int $index) : ?Item{
-		return isset($this->creative[$index]) ? clone $this->creative[$index] : null;
+		return isset($this->items[$index]) ? clone $this->items[$index]->item : null;
 	}
 
 	public function getItemIndex(Item $item) : int{
-		foreach($this->creative as $i => $d){
-			if($item->equals($d, true, false)){
+		foreach($this->items as $i => $d){
+			if($item->equals($d->item, true, false)){
 				return $i;
 			}
 		}
@@ -97,12 +139,79 @@ final class CreativeInventory{
 	 * Adds an item to the creative menu.
 	 * Note: Players who are already online when this is called will not see this change.
 	 */
-	public function add(Item $item) : void{
-		$this->creative[] = clone $item;
+	public function add(Item $item, int $groupId) : void{
+		$groupedItem = new CreativeGroupedItem();
+
+		$groupedItem->item = clone $item;
+		$groupedItem->groupId = $groupId;
+
+		$this->items[] = $groupedItem;
 		$this->onContentChange();
 	}
 
 	/**
+	 * Adds a group to the creative menu.
+	 * Note: Players who are already online when this is called will not see this change.
+	 */
+	public function addGroup(int $categoryId, string $categoryName, ?Item $icon = null) : int{
+		$group = new CreativeItemGroup();
+
+		$group->categoryId = $categoryId;
+		$group->categoryName = $categoryName;
+		$group->icon = $icon;
+
+		$this->groups[] = $group;
+		$this->onContentChange();
+
+		return count($this->groups) - 1;
+	}
+
+	/**
+	 * Removes an item from the creative menu.
+	 * Note: Players who are already online when this is called will not see this change.
+	 */
+	public function remove(Item $item) : void{
+		$index = $this->getItemIndex($item);
+		if($index !== -1){
+			unset($this->items[$index]);
+			$this->onContentChange();
+		}
+	}
+
+	/**
+	 * Removes a group from the creative menu.
+	 * Note: Players who are already online when this is called will not see this change.
+	 */
+	public function removeGroup(int $groupId) : void{
+		foreach($this->items as $d){
+			if($d->groupId === $groupId){
+				throw new \InvalidArgumentException("Cannot remove group with items in it");
+			}
+		}
+
+		foreach($this->items as $i => $d){
+			if($d->groupId === $groupId){
+				unset($this->items[$i]);
+			}
+		}
+		$this->onContentChange();
+	}
+
+	public function contains(Item $item) : bool{
+		return $this->getItemIndex($item) !== -1;
+	}
+
+	/** @phpstan-return ObjectSet<\Closure() : void> */
+	public function getContentChangedCallbacks() : ObjectSet{
+		return $this->contentChangedCallbacks;
+	}
+
+	private function onContentChange() : void{
+		foreach($this->contentChangedCallbacks as $callback){
+			$callback();
+		}
+	}
+}	/**
 	 * Removes an item from the creative menu.
 	 * Note: Players who are already online when this is called will not see this change.
 	 */
